5e986dfb40dd84bcbed1cadb25435059343470c9
Siddhi Nitin Chavan <chavansiddhi487@gmail.com>
2026-01-13 22:18:10 +0530

    Add files via upload

---
+++ function practise/script.js
@@ -0,0 +1,55 @@
+// ----- function declartion
+// function siddhi(){
+//     console.log("Good girl");
+// 
+// siddhi();
+
+
+
+// ---- Function expression
+// let siddhi = function(){
+// console.log("heieieiiee");
+// }
+// siddhi();
+
+
+//fat arrow function 
+// let fnc = ()=>{
+// console.log("hihihuyewg");
+// }
+// fnc();
+
+// parammeter and agrument
+// function dance(s){
+//     console.log(` ${s} is dancing`);
+// }
+// dance("Sidddhi");
+// dance("vinay");
+
+// function add(v1,v2){ //parameter
+//     console.log(v1 + v2);
+// }
+// add(1,2);  // argument
+// add(2,3);
+// add(4,4,4);
+
+// -----default parameter
+// function add(v1=1,v2=2){ 
+//     console.log(v1 + v2);
+// }
+// add(); 
+
+//----- rest or spread "..."
+// function abc(a,b,c,...val){
+//     console.log(a,b,c,val);
+// }
+// abc(1,2,3,4,4,5,5,5);
+
+//------return value 
+// function abcd(){
+//     return 12;
+// }
+// let val = abcd();
+// console.log(val);
+
+// first class function
