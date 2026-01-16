5fb2195e522f788ac2e890f118e394b317a6f5de
Siddhi Nitin Chavan <chavansiddhi487@gmail.com>
2026-01-16 23:07:42 +0530

    Add files via upload

---
+++ array_js.js
@@ -0,0 +1,8 @@
+let arr = [10,20,30,40];
+
+    // console.log(arr[0]);
+
+arr.pop();
+arr.shift(); // remove the first element ie from index one 
+arr.unshift(0);
+arr.splice(2,1);  //(index,no of value to remove)
