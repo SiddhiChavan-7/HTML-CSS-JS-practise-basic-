f1d5a94c344050f67e473c68efa46564dbabbf94
Siddhi Nitin Chavan <chavansiddhi487@gmail.com>
2026-01-18 18:42:14 +0530

    Add files via upload

---
+++ operations on array/script.js
@@ -0,0 +1,54 @@
+// map is used when we want to make an new array and making the new array and the new array keeps the values 
+// let arr = [11, 62, 3, 4, 25];
+
+// let newarr = arr.map(function(val) {
+//     if (val > 10) return val;
+//     else return null; // or 0, or some placeholder
+// });
+
+// console.log(newarr);
+
+// reduce 
+
+// let arr = [1,2,3,4,5,6];
+
+// let ans = arr.reduce(function(accumlator,val){
+//     return accumlator + val;
+// }, 0);
+// console.log(ans)
+
+// find
+
+// let arr = [1,2,3,4,5,6,1];
+
+// let ans = arr.find(function(val){
+//  return val ===1;
+// });
+
+//some
+//  let arr = [10,90,87,34,87]
+//  let ans = arr.some(function(val){
+//       return val < 25;
+//  })
+//  console.log(ans);
+
+// every
+// let arr = [10,90,87,34,87]
+// let ans = arr. every (function(val){
+//     console.log (val>30);
+// })
+
+// destructing 
+// let arr = [1,2,3,4,5];
+// let [a,b, ,c] = arr;
+// console.log(a, b, c);
+
+// shadowing
+// let arr = [1,2,3,4,5,6,7,8,9]
+// let arr2 = [...arr];
+// console.log(arr2);
+
+// create an array with 3 friute and print the second friut
+
+let fruit = ["apple","banana","grapes"];
+console.log(fruit[1]);
